94d7435cdd4d58d74dd8fd6287bfff2a4ef40a24
weilongyun <weilongyun@360.cn>
2024-05-07 18:34:39 +0800

    product_models

---
+++ product/backend/datamodels/product.go
@@ -0,0 +1,9 @@
+package datamodels
+
+type Product struct {
+	ID           int64  `json:"product_id" sql:"product_id"`
+	ProductName  string `json:"product_name" sql:"product_name"`
+	ProductNum   int64  `json:"product_num" sql:"product_num"`
+	ProductImage string `json:"image" sql:"image"`
+	ProductPrice string `json:"product_price" sql:"product_price"`
+}
